420e78ec7e850eee811678870ee7331746bfdebd
Phil Rigovanov <phil@rigovanov.ru>
2017-10-29 00:20:06 +0300

    js compressed

--- assets/js/vunits.js
+++ assets/js/vunits.js
@@ -3,34 +3,4 @@
  * Requires StyleFix from -prefix-free http://leaverou.github.com/prefixfree/
  * @author Lea Verou
  */
-
-(function() {
-
-if(!window.StyleFix) {
-	return;
-}
-
-// Feature test
-var dummy = document.createElement('_').style,
-	units = ['vw', 'vh', 'vm'].filter(function(unit) {
-	dummy.width = '';
-	dummy.width = '10' + unit;
-	return !dummy.width;
-});
-
-StyleFix.register(function(css) {
-	var w = innerWidth, h = innerHeight, m = Math.min(w,h);
-	
-	return css.replace(RegExp('\\b(\\d+)(' + units.join('|') + ')\\b', 'gi'), function($0, num, unit) {
-		switch (unit) {
-			case 'vw':
-				return (num * w / 100) + 'px';
-			case 'vh':
-				return num * h / 100 + 'px';
-			case 'vm':
-				return num * m / 100 + 'px';
-		}
-	});
-});
-
-})();
+n(){if(window.StyleFix){var e=document.createElement("_").style,t=["vw","vh","vm"].filter(function(t){return e.width="",e.width="10"+t,!e.width});StyleFix.register(function(e){var n=innerWidth,i=innerHeight,r=Math.min(n,i);return e.replace(RegExp("\\b(\\d+)("+t.join("|")+")\\b","gi"),function(e,t,c){switch(c){case"vw":return t*n/100+"px";case"vh":return t*i/100+"px";case"vm":return t*r/100+"px"}})})}}();

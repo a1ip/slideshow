f4c025c6f7889bc07bc0e3b4081dc4722ec18a4a
Phil Rigovanov <phil@rigovanov.ru>
2017-10-29 00:23:55 +0300

    js compressed

--- assets/js/vunits.js
+++ assets/js/vunits.js
@@ -3,4 +3,4 @@
  * Requires StyleFix from -prefix-free http://leaverou.github.com/prefixfree/
  * @author Lea Verou
  */
-n(){if(window.StyleFix){var e=document.createElement("_").style,t=["vw","vh","vm"].filter(function(t){return e.width="",e.width="10"+t,!e.width});StyleFix.register(function(e){var n=innerWidth,i=innerHeight,r=Math.min(n,i);return e.replace(RegExp("\\b(\\d+)("+t.join("|")+")\\b","gi"),function(e,t,c){switch(c){case"vw":return t*n/100+"px";case"vh":return t*i/100+"px";case"vm":return t*r/100+"px"}})})}}();
+function(){if(window.StyleFix){var e=document.createElement("_").style,t=["vw","vh","vm"].filter(function(t){return e.width="",e.width="10"+t,!e.width});StyleFix.register(function(e){var n=innerWidth,i=innerHeight,r=Math.min(n,i);return e.replace(RegExp("\\b(\\d+)("+t.join("|")+")\\b","gi"),function(e,t,c){switch(c){case"vw":return t*n/100+"px";case"vh":return t*i/100+"px";case"vm":return t*r/100+"px"}})})}}();

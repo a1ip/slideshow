4481905f2b0aeb8d37f8aeb26dba671ad8ecba05
Philippe Rigovanov <phil@rigovanov.ru>
2017-10-28 22:42:37 +0300

    Update show.js

--- assets/js/show.js
+++ assets/js/show.js
@@ -77,19 +77,3 @@
   };
   window.FX = FX;
 })()
-function changeImage(el, fade) {
-  FX.fadeOut(el, {
-    duration: fade,
-    delay: 50,
-    complete: function() {
-      el.style.backgroundImage =
-        'url(https://source.unsplash.com/collection/1062065?sig=' +
-        Math.random() + ')';
-      FX.fadeIn(el, {
-        duration: fade * 3,
-        delay: 50,
-        complete: function(){}
-      });
-    }
-  })
-}

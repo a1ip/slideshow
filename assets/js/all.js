36ed906da5beead8406c9f7c0235e191006311fd
Phil Rigovanov <phil@rigovanov.ru>
2017-10-30 16:44:33 +0300

    combining js

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,4 +1,16 @@
 ---
+# about prefixfree.js
+#
+# StyleFix 1.0.3 & PrefixFree 1.0.7
+# @author Lea Verou
+# MIT license
+#
+# about vunits.js
+#
+# Polyfill for the vw, vh, vm units
+# Requires StyleFix from -prefix-free http://leaverou.github.com/prefixfree/
+# @author Lea Verou
+#
 layout: null
 ---
 {% include_relative prefixfree.js %}{% include_relative vunits.js %}{% include_relative fade.js %}
